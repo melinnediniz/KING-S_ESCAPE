d474c1a404e9dccf8545f8e8d9356c278621b931
Níckolas Javier Santos Lívero <njsl.snf21@uea.edu.br>
2022-05-14 22:32:45 -0400

    pause button correction bug

--- CastleDungeon/Assets/Scripts/GameController.cs
+++ CastleDungeon/Assets/Scripts/GameController.cs
@@ -10,6 +10,8 @@ public class GameController : MonoBehaviour
     public static GameController Instance;
     public GameObject gameOver;
     public GameObject pause;
+    public GameObject restartButton;
+    public GameObject pauseButton;
     public bool isPaused;
  
     void Start()
@@ -41,12 +43,15 @@ public class GameController : MonoBehaviour
             Time.timeScale = 0f;
             AudioListener.pause = true;
             pause.SetActive(true);
+            restartButton.SetActive(false);
+            pauseButton.SetActive(true);
         }
         else 
         {
             Time.timeScale = 1;
             AudioListener.pause = false;
             pause.SetActive(false);
+            restartButton.SetActive(true);
         }
     }
 
